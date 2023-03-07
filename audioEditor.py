5330d5672c6d4cb9a4b8e7e14c38300087528a24
Khent Mark Dahay <khentmark27dahay@gmail.com>
2023-03-07 20:36:15 +0800

    add specific dir for final edited files

--- audioEditor.py
+++ audioEditor.py
@@ -9,41 +9,50 @@ class AudioTrimmer:
         self.combined_file = None
 
     def trim_files(self, end_time, start_time=0):
-        for file in os.listdir(self.folder_path):
-            
-            if file.endswith(".wav"):
-                # load the audio file
-                audio = AudioSegment.from_file(os.path.join(self.folder_path, file))
+        try:
+            for file in os.listdir(self.folder_path):
                 
-                end_time_initial = len(audio) - (end_time * 1000)
-                print(f"  end_time_initial {end_time_initial}")
-                print(f"len(audio) {len(audio)}")
+                if file.endswith(".wav"):
+                    # load the audio file
+                    audio = AudioSegment.from_file(os.path.join(self.folder_path, file))
+                    
+                    end_time_initial = len(audio) - (end_time * 1000)
+                    print(f"  end_time_initial {end_time_initial}")
+                    print(f"len(audio) {len(audio)}")
 
-                # trim the audio file
-                trimmed_audio = audio[start_time:end_time_initial]
+                    # trim the audio file
+                    trimmed_audio = audio[start_time:end_time_initial]
 
-                # export the trimmed audio to a new file
-                trimmed_file = os.path.join(self.folder_path, f"trimmed_{file}")
-                trimmed_audio.export(trimmed_file, format=file.split(".")[-1])
+                    # export the trimmed audio to a new file
+                    trimmed_file = os.path.join(self.folder_path, f"trimmed_{file}")
+                    trimmed_audio.export(trimmed_file, format=file.split(".")[-1])
 
-                self.trimmed_files.append(trimmed_file)
+                    self.trimmed_files.append(trimmed_file)
+        except:
+            pass
 
-    def combine_files(self):
+    def combine_files(self, dir_path):
+
+        if dir_path == "":
+            dir_path = self.folder_path
+
         if not self.trimmed_files:
             print("No trimmed files found.")
             return
-
+        try:
         # load the first audio file
-        combined_audio = AudioSegment.from_file(self.trimmed_files[0])
+            combined_audio = AudioSegment.from_file(self.trimmed_files[0])
 
-        # concatenate the rest of the audio files
-        for file in self.trimmed_files[1:]:
-            audio = AudioSegment.from_file(file)
-            combined_audio += audio
+            # concatenate the rest of the audio files
+            for file in self.trimmed_files[1:]:
+                audio = AudioSegment.from_file(file)
+                combined_audio += audio
 
-        # export the combined audio to a new file
-        self.combined_file = os.path.join(self.folder_path, f"{self.folder_path}combined_file.wav")
-        combined_audio.export(self.combined_file, format="wav")
+            # export the combined audio to a new file
+            self.combined_file = os.path.join(dir_path, f"{self.folder_path}combined_file.wav")
+            combined_audio.export(self.combined_file, format="wav")
+        except:
+            pass
 
 if __name__ == "__main__":
     folder_path = "audiofile"
